8032e0b7bc7dc060de7d450f85d8934dd8737604
安田 学 <19850318m.yasuda@gmail.com>
2015-08-12 21:44:46 +0900

    jadeの導入。run-sequenceを使用した処理に大幅変更。エラー時のコンパイル停止を防止して、デスクトップ通知をするように変更。

--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,99 @@
 var gulp = require('gulp');
+var jade = require('gulp-jade');
+var marked = require('marked');
 var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var notify = require("gulp-notify");
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 var csscomb = require('gulp-csscomb');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var browserSync = require('browser-sync');
+var runSequence = require('run-sequence');
 
-var paths = {
-  'rootDir': './source',
-  'htmlSrc': 'source/*.html',
-  'scssSrc': 'source/scss/*.scss',
-  'cssSrc': 'source/css',
-  'distDir': 'source/dist'
+
+/**
+ * コンパイル前のソースへのパス。
+ */
+var source = {
+  'root': 'source/',
+  'jade': 'source/**/',
+  'sass': 'source/asset/scss/*.scss'
 };
 
-gulp.task('browser-sync', function() {
-  browserSync({
-    server: {
-      baseDir: paths.rootDir
-    }
-  });
-});
+/**
+ * コンパイル後に出力するパス。
+ */
+var build = {
+  'root': 'build/',
+  'css': 'build/css/',
+  'html': 'build/'
+};
 
-gulp.task('bs-reload', function () {
-  browserSync.reload();
-});
-
-gulp.task('sass', function() {
-  return gulp.src(paths.scssSrc)
-  .pipe(sass().on('error', sass.logError))
-  .pipe(autoprefixer({
-    browsers: ['last 2 versions'],
+/**
+ * jadeをhtmlにコンパイル。
+ */
+gulp.task('jade', function() {
+  gulp.src([source.jade + '*.jade', '!' + source.jade + '_*.jade'])
+  .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+  .pipe(jade({
+    pretty: true
   }))
-  .pipe(sass())
-  .pipe(csscomb())
-  .pipe(sourcemaps.init())
-  .pipe(sourcemaps.write())
-  .pipe(gulp.dest(paths.cssSrc))
+  .pipe(gulp.dest(build.html))
   .pipe(browserSync.reload({stream: true}));
 });
 
-gulp.task('default', ['sass', 'browser-sync'], function() {
-  gulp.watch(paths.scssSrc, ['sass']);
-  gulp.watch(paths.htmlSrc, ['bs-reload']);
-});
-
-gulp.task('minify-css', function() {
-  return gulp.src(paths.scssSrc)
+/**
+ * .scssを.cssにコンパイル。
+ * 圧縮する場合は`gulp css-minify`を実行します。
+ */
+gulp.task('sass', function(){
+  return gulp.src(source.sass)
+  .pipe(sourcemaps.init())
+  .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+  .pipe(sass().on('error', sass.logError))
   .pipe(autoprefixer({
-    browsers: ['last 2 versions'],
+    browsers: ['last 3 versions'],
   }))
   .pipe(sass())
   .pipe(csscomb())
-  .pipe(sourcemaps.init())
   .pipe(sourcemaps.write())
-  .pipe(gulp.dest(paths.distDir + '/css'))
-  .pipe(minifyCss())
+  .pipe(gulp.dest(build.css))
+  .pipe(browserSync.reload({stream: true}));
+});
+
+/**
+ * ローカルサーバーの起動後、各ファイルの監視をする。
+ */
+gulp.task('browser-sync', function() {
+  browserSync({
+    server: {
+      baseDir: build.root
+    }
+  })
+  gulp.watch(source.jade + '*.jade', ['jade']);
+  gulp.watch(source.sass, ['sass']);
+});
+
+
+/**
+ * jade, sassのコンパイルの監視とライブリロード。
+ */
+gulp.task('watch', function() {
+  runSequence(
+    ['jade', 'sass'], 'browser-sync'
+  );
+});
+
+/**
+ * コンパイルしているCSSファイルを圧縮（コピー）して.minを付与する。
+ */
+gulp.task('css-minify', function(){
+  return gulp.src(build.css + '/*.css')
+  .pipe(sourcemaps.init())
   .pipe(rename({suffix: '.min'}))
-  .pipe(gulp.dest(paths.distDir + '/css'));
+  .pipe(minifyCss())
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest(build.css));
 });
